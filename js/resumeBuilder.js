0b62dabe1b595f040eed9ebcb0c53e17d6273008
Paul <paulsur@gmail.com>
2016-01-24 09:58:20 -0700

    Beginning to work on the resume with objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+  "name" : "Paul S",
+  "role" : "Novice Web Developer",
+  "contacts" : {
+    "mobile" : "403-555-5555",
+    "email" : "paulsur at gmail.com",
+    "github" : "paulsur",
+    "location" : "Calgary"
+  }
+  "welcomeMessage" : "Welcome to this resume website!",
+  "skills" : [ "web development", "Microsoft Excel", "grilling steak"],
+  "bioPic" : "https://www.365ninja.com/wp-content/themes/senorclippy/images/ninja-logo.png"
+}
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
